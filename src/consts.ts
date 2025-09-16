0bdf15927758ca04aafb3da7ea10093e4953819b
Oleksiy Zelenyuk <o@zelen.uk>
2025-09-16 18:02:29 +0200

    second commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,22 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "JernieFit - Entrenamiento Personalizado";
+export const SITE_DESCRIPTION = "Transforma tu cuerpo y mente con entrenamiento personalizado. Soy Jernie Richard, entrenadora certificada con más de 15 años de experiencia ayudando a personas a alcanzar sus objetivos de salud y bienestar.";
+
+// Jernie's personal information
+export const TRAINER_NAME = "Jernie Richard";
+export const TRAINER_AGE = "31 años";
+export const TRAINER_LOCATION = "Valencia, España";
+export const TRAINER_ORIGIN = "Dominicana";
+export const EXPERIENCE_YEARS = "15+ años";
+
+// Contact information
+export const WHATSAPP_NUMBER = "+34-XXX-XXX-XXX"; // Update with real number
+export const EMAIL = "jernie@jerniefit.com"; // Update with real email
+
+// Social media
+export const INSTAGRAM = "https://www.instagram.com/jerniefit";
+export const YOUTUBE = "https://www.youtube.com/@jerniefit";
+export const LINKEDIN = "https://www.linkedin.com/in/jernie-richard-49b237370";
+export const TIKTOK = "@jerniefit"; // Update with real handle
